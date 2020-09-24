c112e9e0d9fa0b8f5afc3a99f8320229f3045904
unknown <s2018s27@e-mirim.hs.kr>
2020-09-24 21:13:46 +0900

    add main img, create board part

---
+++ src/BoardDetail.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import { Table, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+axios.defaults.withCredentials = true;
+const headers = { withCredentials: true };
+
+class BoardDetail extends Component {
+  state = {
+    board: []
+  };
+
+  componentDidMount() {
+    if (this.props.location.query !== undefined) {
+      this.getDetail();
+    } else {
+      window.location.href = "/";
+    }
+  }
+
+  deleteBoard = _id => {
+    const send_param = {
+      headers,
+      _id
+    };
+    if (window.confirm("게시글을 삭제하시겠습니까?")) {
+      axios
+        .post("http://localhost:8080/board/delete", send_param)
+
+        .then(returnData => {
+          alert("게시글이 삭제 되었습니다!");
+          window.location.href = "/";
+        })
+
+        .catch(err => {
+          console.log(err);
+          alert("게시글 삭제에 실패했습니다!");
+        });
+    }
+  };
+
+  getDetail = () => {
+    const send_param = {
+      headers,
+      _id: this.props.location.query._id
+    };
+    const marginBottom = {
+      marginBottom: 5
+    };
+    axios
+      .post("http://localhost:8080/board/detail", send_param)
+
+      .then(returnData => {
+        if (returnData.data.board[0]) {
+          const board = (
+            <div>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>{returnData.data.board[0].title}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td
+                      dangerouslySetInnerHTML={{
+                        __html: returnData.data.board[0].content
+                      }}
+                    ></td>
+                  </tr>
+                </tbody>
+              </Table>
+              <div>
+                <NavLink
+                  to={{
+                    pathname: "/boardWrite",
+                    query: {
+                      title: returnData.data.board[0].title,
+                      content: returnData.data.board[0].content,
+                      _id: this.props.location.query._id
+                    }
+                  }}
+                >
+                  <Button block style={marginBottom}>
+                    게시글 수정
+                  </Button>
+                </NavLink>
+                <Button
+                  block
+                  onClick={this.deleteBoard.bind(
+                    null,
+                    this.props.location.query._id
+                  )}
+                >
+                  게시글 삭제
+                </Button>
+              </div>
+            </div>
+          );
+          this.setState({
+            board: board
+          });
+        } else {
+          alert("게시글 조회를 실패했습니다!");
+        }
+      })
+     
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const divStyle = {
+      margin: 50
+    };
+    return <div style={divStyle}>{this.state.board}</div>;
+  }
+}
+
+export default BoardDetail;
